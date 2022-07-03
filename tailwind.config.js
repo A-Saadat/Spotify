94b00ea629be98c80184d77e21b339d31093c20a
Ali Saadat <ali.saadat.4950@gmail.com>
2022-07-03 12:28:18 +0430

    Setup Project :)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./Src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        circular : 'Circular Std',
+      },
+      colors: {
+        customIndigo : '#493590',
+        primetyGreen: '#1DB954',
+        primetyBlack: '#191414',
+        primetyWhite: '#F5F5F5',
+      }
+    },
+  },
+  plugins: [],
+}
